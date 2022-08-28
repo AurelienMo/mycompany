71999f43baa57444ec82d281bf7a67acdb52a06a
Aurelien Morvan <aurelien.morvan@smartpush.fr>
2022-08-28 10:55:00 +0200

    feat(Authentication): API to authenticate
    - Implement API to obtain token

---
+++ src/Subscribers/AuthenticationSubscribers.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MyCompany\Subscribers;
+
+use Gesdinet\JWTRefreshTokenBundle\Generator\RefreshTokenGeneratorInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTExpiredEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationFailureResponse;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticationSubscribers
+{
+    public function __construct(private RefreshTokenGeneratorInterface $refreshTokenGenerator) {}
+
+    public function onJwtExpired(JWTExpiredEvent $event)
+    {
+        /** @var JWTAuthenticationFailureResponse $response */
+        $response = $event->getResponse();
+        $response->setMessage("Jeton expiré");
+    }
+
+    public function onInvalidCredentials(AuthenticationFailureEvent $event)
+    {
+        $response = new JWTAuthenticationFailureResponse('Identifiants invalides.', Response::HTTP_UNAUTHORIZED);
+        $event->setResponse($response);
+    }
+
+    public function onSuccess(AuthenticationSuccessEvent $event)
+    {
+        $extraData = [
+            'id' => $event->getUser()->getId()->toString(),
+            'refresh_token' => $this->refreshTokenGenerator->createForUserWithTtl($event->getUser(), 2592000)
+        ];
+
+        $event->setData(array_merge($event->getData(), $extraData));
+    }
+}
